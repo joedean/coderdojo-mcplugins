8d8a7f3c8b76844b66408fdb0722326bafb860d4
Joseph Dean <joe@on-site.com>
2014-02-14 23:20:02 -0800

    Make paths platform independent

--- lib/check_environment.rb
+++ lib/check_environment.rb
@@ -1,11 +1,10 @@
+require 'tempfile'
 require 'fileutils'
 require 'open-uri'
-require 'zlib'
 
 class CheckEnvironment
-  VERSION="0.1"
-  FILE_SEPARATOR = java.lang.System.getProperty "file.separator"
-  CODERDOJO_DIR = "~#{FILE_SEPARATOR}.coderdojo"
+  VERSION = "0.1"
+  APP_ROOT = File.join File.dirname(__FILE__), '..'
 
   def run
     prompt_for_user_name
@@ -24,13 +23,7 @@ class CheckEnvironment
       @name = prompt_user_name "Common you need to do better than that! Try entering your Minecraft user name again. "
     end
     raise "Failed to provide a valid Minecraft user name.  Please try again!" if @name.empty?
-    puts "Awesome!  Thanks #{@name}!  Please be patience while I inspect your environment..."
-  end
-
-  def prompt_user_name request_msg
-    print request_msg
-    user_input = gets
-    user_input.strip
+    puts "Please be patience while I inspect your environment..."
   end
 
   def check_java
@@ -39,6 +32,85 @@ class CheckEnvironment
     success "JDK version #{javac_version}"
   end
 
+  def check_bukkit
+    server_path = File.join(APP_ROOT,  'server')
+    craftbukkit_path = File.join server_path, 'craftbukkit.jar'
+
+    mkdir server_path unless Dir.exists? server_path
+    #TODO: check file size of craftbukkit.jar
+    unless File.exists? craftbukkit_path
+      puts "Downloading craftbukkit.jar.  Please wait..."
+      download "http://dl.bukkit.org/latest-rb/craftbukkit.jar", craftbukkit_path
+    end
+    success "Minecraft server Craftbukkit"
+  end
+
+  def check_sublime
+    raise "Install sublime_text and make sure it's in your PATH" unless which "sublime_text"
+    success "Sublime Text"
+  end
+
+  def check_forge
+    forge = 'forge-1.6.4-9.11.1.965'
+    minecraft_path = File.join APP_ROOT, '..', '.minecraft'
+    coderdojo_path = File.join minecraft_path, 'versions', 'coderdojo'
+    forge_path = File.join minecraft_path, 'versions', forge
+    forge_installer = File.join APP_ROOT, 'minecraft', "#{forge}-installer.jar"
+    json_path = File.join coderdojo_path, 'coderdojo.json'
+
+    puts "When the simple forge installer dialog comes up select 'Install client' and click 'Ok'"
+    %x[java -jar #{forge_installer}]
+    mkdir coderdojo_path unless Dir.exists? coderdojo_path
+    unless File.exists? File.join(coderdojo_path, 'coderdojo.json')
+      FileUtils.cp File.join(forge_path, "#{forge}.jar"), File.join(coderdojo_path, 'coderdojo.jar')
+      FileUtils.cp File.join(forge_path, "#{forge}.json"), json_path
+    end
+
+    temp_file = Tempfile.new('coderdojo.json')
+    begin
+      File.open(json_path, 'r') do |file|
+        file.each_line do |line|
+          if (!!(/"id": ".*",/ =~ line))
+            temp_file.puts line.gsub(/1\.6\.4-Forge9\.11\.1\.965/, 'coderdojo')
+          else
+            temp_file.puts line
+          end
+        end
+      end
+      temp_file.rewind
+      FileUtils.mv(temp_file.path, json_path)
+    ensure
+      temp_file.close
+      temp_file.unlink
+    end
+  end
+
+  def check_computer_craft
+    mods_dir = File.join APP_ROOT, '..', '.minecraft', 'mods'
+    unless File.exists? File.join(mods_dir, 'ComputerCraft1.58.zip')
+      FileUtils.cp(File.join(APP_ROOT, 'minecraft', 'ComputerCraft1.58.zip'),
+                   File.join(mods_dir, 'ComputerCraft1.58.zip'))
+    end
+  end
+
+  def generate_key
+    puts "-------------------------"
+    decoded_key = "Version #{VERSION} - #{@name}"
+    puts decoded_key
+    encoded_key = [decoded_key].pack "u"
+    puts encoded_key
+    puts "-------------------------"
+  end
+
+  ### Utility Methods ###
+  # Manages user input
+  def prompt_user_name request_msg
+    print request_msg
+    user_input = gets
+    user_input.strip
+  end
+
+  # Java version helper methods
   def java_versions_match?
     java_version == javac_version
   end
@@ -51,16 +123,12 @@ class CheckEnvironment
     %x[javac -version 2>&1 | awk -F ' ' '{print $2}'].strip
   end
 
-  def check_bukkit
-    mkdir "server"
-    # check file size of craftbukkit.jar
-    unless File.exists? "server#{FILE_SEPARATOR}craftbukkit.jar"
-      puts "Downloading craftbukkit.jar.  Please wait..."
-      download "http://dl.bukkit.org/latest-rb/craftbukkit.jar", "server#{FILE_SEPARATOR}craftbukkit.jar"
-    end
-    success "Minecraft server Craftbukkit"
+  # Delivers success message when environment check completes successfully
+  def success message
+    puts "#{message} is installed correctly!"
   end
 
+  # Download url to a specified location
   def download url, location
     File.open(location, "wb") do |saved_file|
       open(url, "rb") do |read_file|
@@ -69,47 +137,9 @@ class CheckEnvironment
     end
   end
 
+  # Helper method for making a directory on filesystem
   def mkdir dir
     FileUtils.mkdir dir unless File.directory? dir
-  end
-
-  def check_sublime
-    raise "Install sublime_text and make sure it's in your PATH" unless which "sublime_text"
-    success "Sublime Text"
-  end
-
-  def check_forge
-    puts "When the simple forge installer dialog comes up select 'Install client' and click 'Ok'"
-    %x[java -jar ~/coderdojo/minecraft/forge-1.6.4-9.11.1.965-installer.jar]
-    FileUtils.mkdir ".minecraft/versions/coderdojo" unless File.directory?(".minecraft/versions/coderdojo")
-    unless File.exists? ".minecraft/versions/coderdojo.jar"
-      FileUtils.cp ".minecraft/versions/1.6.4-Forge9.11.1.965/1.6.4-Forge9.11.1.965.jar", ".minecraft/versions/coderdojo/coderdojo.jar"
-      FileUtils.cp ".minecraft/versions/1.6.4-Forge9.11.1.965/1.6.4-Forge9.11.1.965.json", ".minecraft/versions/coderdojo/coderdojo.json"
-    end
-  end
-
-  def check_computer_craft
-    FileUtils.mv('coderdojo/minecraft/ComputerCraft1.58.zip' '.minecraft/mods/') unless File.exists? ".minecraft#{FILE_SEPARATOR}mods#{FILE_SEPARATOR}ComputerCraft1.58.zip"
-  end
-
-  def generate_key
-    puts "-------------------------"
-    value_to_encrypt = "Version #{VERSION} - #{@name}"
-    puts value_to_encrypt
-    encrypted_value = deflate(value_to_encrypt)
-    puts encrypted_value
-    puts "-------------------------"
-  end
-
-  def deflate(string, level=Zlib::BEST_SPEED)
-    z = Zlib::Deflate.new(level)
-    dst = z.deflate(string, Zlib::FINISH)
-    z.close
-    dst
-  end
-
-  def success message
-    puts "#{message} is installed correctly!"
   end
 
   # Cross-platform way of finding an executable in the $PATH
